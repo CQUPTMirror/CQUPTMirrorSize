1ece09e2252024dfcb7fcb5a8cf97703ce6d5346
Lich2013 <longzy2013@gmail.com>
2016-03-20 00:28:48 +0800

    change to threading

--- record.py
+++ record.py
@@ -3,12 +3,8 @@
 import os
 import json
 import time
-dir = '/data/mirror'
-size = 0
-data = {'mirror_list':'', 'update_time':0, 'update_cost_time':0}
-mirrorsSize = []
-start_time = time.time()
-for sonDir in os.listdir(dir):
+import threading
+def getSize(mirrorsSize, sonDir):
     mirror = {'mirrorName':'', 'storage':'', 'lastUpdate':''}
     size = 0
     try:
@@ -24,14 +20,23 @@ for sonDir in os.listdir(dir):
     mirror['lastUpdate'] = int(time.time())
     print mirror
     mirrorsSize.append(mirror)
-end_time = time.time()
-data['mirror_list'] = mirrorsSize
-data['update_time'] = int(time.time())
-data['update_cost_time'] = int(end_time - start_time)
-f = open('main.json', 'w', 1024)
-f.write(json.dumps(data))
-f.close()
-log = open('success.log', 'a', 1024)
-currenttime = time.strftime('%Y-%m-%d %X', time.localtime( time.time() ) )
-log.write('[Info] Updated at: ' + str(currenttime) + ' cost ' + str(data['update_cost_time']) + "s\n")
-log.close()
+
+if __name__ == '__main__':
+    dir = '/data/mirror'
+    size = 0
+    data = {'mirror_list':'', 'update_time':0, 'update_cost_time':0}
+    mirrorsSize = []
+    start_time = time.time()
+    for sonDir in os.listdir(dir):
+        threading.Thread(target=getSize, args=(mirrorsSize, sonDir,)).start()
+    end_time = time.time()
+    data['mirror_list'] = mirrorsSize
+    data['update_time'] = int(time.time())
+    data['update_cost_time'] = int(end_time - start_time)
+    f = open('main.json', 'w', 1024)
+    f.write(json.dumps(data))
+    f.close()
+    log = open('success.log', 'a', 1024)
+    currenttime = time.strftime('%Y-%m-%d %X', time.localtime( time.time() ) )
+    log.write('[Info] Updated at: ' + str(currenttime) + ' cost ' + str(data['update_cost_time']) + "s\n")
+    log.close()
