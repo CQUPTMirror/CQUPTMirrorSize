0ff92d657ab1f400f624f2345b135471a3bedb64
Lich2013 <longzy2013@gmail.com>
2016-03-21 18:54:02 +0800

    change to absolute path

--- record.py
+++ record.py
@@ -11,7 +11,7 @@ def getSize(mirrorsSize, sonDir):
         for root, dirs, files in os.walk(os.path.join(dir, sonDir)):
             size += sum([os.path.getsize(os.path.join(root, name)) for name in files])
     except OSError, e:
-            log = open('error.log', 'a', 1024)
+            log = open('/data/config/error.log', 'a', 1024)
             currenttime = time.strftime('%Y-%m-%d %X', time.localtime( time.time() ) )
             log.write('[Error]: ' + str(currenttime) + ' ' + str(e) + "\n")
             log.close()
@@ -37,10 +37,10 @@ if __name__ == '__main__':
     data['mirror_list'] = mirrorsSize
     data['update_time'] = int(time.time())
     data['update_cost_time'] = int(end_time - start_time)
-    f = open('main.json', 'w', 1024)
+    f = open('/data/config/main.json', 'w', 1024)
     f.write(json.dumps(data))
     f.close()
-    log = open('success.log', 'a', 1024)
+    log = open('/data/config/success.log', 'a', 1024)
     currenttime = time.strftime('%Y-%m-%d %X', time.localtime( time.time() ) )
     log.write('[Info] Updated at: ' + str(currenttime) + ' cost ' + str(data['update_cost_time']) + "s\n")
     log.close()
